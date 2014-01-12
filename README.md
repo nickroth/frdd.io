f13caae8e70e3c02e7159aaab7ba3b631945234d
Nick Roth <nicholas.roth.1@gmail.com>
2014-01-12 11:37:45 -0500

    Realized some updates were missing from repo. Updated README to start indexing projects.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-frdd.io
-=======
+# frdd.io
+
+*The latest revolution in acronym based development methodologies*
+
+## What's here?
+Just a simple repo to collect larger code examples from [my blog](www.frdd.io).
+
+## Project Listing
+
+- [mustache.codegen](http://www.frdd.io/code-generation-with-tom-selleck/)
+
+	Using Mustache templates and Java Reflection to generate code.
+
--- mustache.codegen/src/main/java/io/frdd/blogs/mustache/codegen/App.java
+++ mustache.codegen/src/main/java/io/frdd/blogs/mustache/codegen/App.java
@@ -5,8 +5,6 @@ import java.io.PrintWriter;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -78,19 +76,20 @@ public class App {
 		}
 	}
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
 
-		Class<OverrideMe> inheritedType = OverrideMe.class;
-		Package inheritedTypePackage = inheritedType.getPackage();
+		// Load up the the parent class.
+		Class<?> inheritedType = Class.forName("io.frdd.blogs.mustache.codegen.OverrideMe");
 		
+		// Initialize the set of Imports and Methods
 		Set<Import> imports = new HashSet<Import>();
-		imports.add(new Import(inheritedTypePackage.getName()));
+		imports.add(new Import(inheritedType.getName()));
 		
 		List<MustacheMethod> methods = new LinkedList<App.MustacheMethod>();
-		
+
+		// Create the top level scope for the template
 		Map<String, Object> scope = new HashMap<String, Object>();
 		scope.put("package", "io.frdd");
-		
 		
 		if (Modifier.isInterface(inheritedType.getModifiers())) {
 			scope.put("inheritenceType", "implements");			
@@ -102,10 +101,10 @@ public class App {
 
 		Method[] parentMethods = inheritedType.getDeclaredMethods();
 		for(Method m : parentMethods) {
-			// We're not interested in overriding methods inherited from Object right now.
+			// We're not interested in overriding methods inherited from Object.
 			if(isObjectMethod(m)) {continue;}
 			
-			//Check for methods we can't override
+			// Check for methods we can't override.
 			if (!canOverride(m)) {continue;}
 
 			String vis = getVisibility(m);
@@ -113,12 +112,12 @@ public class App {
 			String returnType = (m.getReturnType().equals(Void.TYPE)) ? null : m.getReturnType().getSimpleName();
 			MustacheMethod method = new MustacheMethod(vis, name, returnType);
 			
-			if (returnType != null) imports.add(new Import(m.getReturnType().getPackage().getName()));
+			if (returnType != null) imports.add(new Import(m.getName()));
 			
 			Class<?>[] paramTypes = m.getParameterTypes();
 			for (Class<?> paramType : paramTypes) {
 				method.addArgument(paramType.getSimpleName());
-				imports.add(new Import(paramType.getPackage().getName()));
+				imports.add(new Import(paramType.getName()));
 			}
 			methods.add(method);
 		}
@@ -140,13 +139,25 @@ public class App {
 		return ""; //Default visibility
 	}
 
+	/**
+	 * Determines if the passed method could be overridden by a subclass. Static, private and final
+	 * methods can not be overridden.
+	 * @param m The Method to test
+	 * @return true iff the passed method can be overridden by a subclass, false otherwise.
+	 */
 	private static boolean canOverride(Method m) {
 		int mod = m.getModifiers();
 		return !(Modifier.isStatic(mod) || Modifier.isPrivate(mod) || Modifier.isFinal(mod));
 	}
 
+	// Create a Set of Methods that all objects inherit from Object 
 	private static final Set<Method> OBJECT_METHODS = new HashSet<Method>(Arrays.asList(Object.class.getMethods()));
 	
+	/**
+	 * Determines if the passed Method is one that is inherited from the Object class.
+	 * @param m The Method to test
+	 * @return true iff the passed Method is one that is inherited from the Object class, false otherwise.
+	 */
 	private static boolean isObjectMethod(Method m) {
 		return OBJECT_METHODS.contains(m);
 	}
